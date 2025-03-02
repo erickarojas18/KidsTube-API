3cac63d7ebd8e6560688b5bd3f7a7ed1f201b3b6
erickarojas18 <125708318+erickarojas18@users.noreply.github.com>
2025-03-02 17:42:43 -0600

    Primer commit - Agregando archivos del proyecto

---
+++ config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
