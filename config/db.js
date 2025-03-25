7635098061ea288e701e41d9c1c5febf2161acfb
erickarojas18 <125708318+erickarojas18@users.noreply.github.com>
2025-03-24 19:38:17 -0600

    login

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+        await mongoose.connect(process.env.DATABASE_URL);
         console.log('MongoDB Connected');
     } catch (error) {
-        console.error(error);
+        console.error('Error al conectar con MongoDB:', error.message);
         process.exit(1);
     }
 };
 
 module.exports = connectDB;
+
